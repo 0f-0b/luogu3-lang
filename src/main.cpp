3a6c272620c50de318135563e4041a6f97f637ea
undefined <sjx233@qq.com>
2021-04-12 00:36:55 +0800

    Add -o, --output

--- src/main.cpp
+++ src/main.cpp
@@ -26,13 +26,15 @@ struct help_impl {
 
 auto main(int argc, char* argv[]) -> int {
   std::ios_base::sync_with_stdio(false);
-  const char* filename;
+  std::string filename;
+  std::string output;
   bool format;
   {
     auto arg_parser = argagg::parser{{
-      {"version", {"-V", "--version"}, "shows the version", 0},
-      {"format", {"-f", "--format"}, "formats the code instead of compiling it", 0},
-      {"help", {"-h", "--help"}, "shows this help message", 0},
+      {"version", {"-V", "--version"}, "show the version", 0},
+      {"output", {"-o", "--output"}, "output file (default: -)", 1},
+      {"format", {"-f", "--format"}, "format the code instead of compiling it", 0},
+      {"help", {"-h", "--help"}, "show this help message", 0},
     }};
     auto help = help_impl{*argv, arg_parser};
     argagg::parser_results args;
@@ -58,26 +60,42 @@ auto main(int argc, char* argv[]) -> int {
       return 2;
     }
     filename = args.pos[0];
+    output = args["output"].as<std::string>("-");
     format = args["format"];
   }
   errno = 0;
-  std::string source = [&]() -> std::string {
-    if (std::strcmp(filename, "-")) {
-      auto in = std::ifstream{filename};
-      return read_fully(in);
-    }
-    filename = "<stdin>";
-    return read_fully(std::cin);
-  }();
+  std::string source;
+  {
+    auto is_std = filename == "-";
+    auto& in = is_std ? std::cin : *new std::ifstream{filename};
+    if (is_std)
+      filename = "<stdin>";
+    source.assign(std::istreambuf_iterator<char>{in}, std::istreambuf_iterator<char>{});
+    if (!is_std)
+      delete &in;
+  }
   if (errno) {
     std::cerr << filename << ": " << std::strerror(errno) << '\n';
     return 1;
   }
   auto result = ud2::luogu3::compile(source);
-  auto error = ud2::luogu3::print_diagnostics(std::cerr, result.diags, filename, source.c_str());
-  if (format)
-    result.prog.emit_source(std::cout);
-  else
-    result.prog.emit_c(std::cout);
+  auto error = ud2::luogu3::print_diagnostics(std::cerr, result.diags, filename.c_str(), source.c_str());
+  errno = 0;
+  {
+    auto is_std = output == "-";
+    auto& out = is_std ? std::cout : *new std::ofstream{output};
+    if (is_std)
+      output = "<stdout>";
+    if (format)
+      result.prog.emit_source(out);
+    else
+      result.prog.emit_c(out);
+    if (!is_std)
+      delete &out;
+  }
+  if (errno) {
+    std::cerr << output << ": " << std::strerror(errno) << '\n';
+    return 1;
+  }
   return error;
 }
